edebab15d17c6df7cc868dd1a1e7fc1cc3ba1f1f
Bernhard Biskup <bernhard@fract-tech.com>
2017-11-12 13:29:14 +0100

    Added command line option '--port'

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,36 @@
 #include "vendor/crow_all.h"
 #include <boost/uuid/uuid.hpp>
 #include <boost/uuid/uuid_generators.hpp>
+#include <boost/program_options.hpp>
 #include <iostream>
 
 #include "blockchain.h"
 
 namespace {
-//const boost::uuids::uuid nodeIdentifier{boost::uuids::random_generator()()};
-const int serverPort{5000};
+const boost::uuids::uuid nodeIdentifier{boost::uuids::random_generator()()};
+const unsigned short defaultServerPort{5000};
 } // namespace
 
-int main() {
+
+namespace bpo = boost::program_options;
+
+int main(int argc, char** argv) {
+  bpo::options_description desc{"Allowed options:"};
+  bool is_help{false};
+  unsigned short serverPort;
+  desc.add_options()("help,help",
+                     bpo::bool_switch(&is_help)->default_value(false),
+                     "Display help message")(
+      "port,p", bpo::value<unsigned short>(&serverPort)->default_value(defaultServerPort),
+      "Server HTTP port");
+  bpo::variables_map vm;
+  bpo::store(bpo::parse_command_line(argc, argv, desc), vm);
+  bpo::notify(vm); // may throw
+
+  if (is_help) {
+    std::cerr << desc << std::endl;
+    return 0;
+  }
   bc::BlockChain blockChain{};
 
   crow::SimpleApp app;
