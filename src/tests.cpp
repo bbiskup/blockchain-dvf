c7db0cb9b919c7118b515af8a0fd196c1199d078
Bernhard Biskup <bernhard@fract-tech.com>
2017-11-14 21:34:47 +0100

    Added tests

--- src/tests.cpp
+++ src/tests.cpp
@@ -28,5 +28,16 @@ TEST_CASE("Block") {
 
   nlohmann::json j;
   bc::to_json(j, block);
-  REQUIRE(j.dump() == R"({"index":10,"previous_hash":"test_hash","proof":99,"timestamp":0,"transactions":[]})");
+  REQUIRE(
+      j.dump() ==
+      R"({"index":10,"previous_hash":"test_hash","proof":99,"timestamp":0,"transactions":[]})");
+}
+
+TEST_CASE("hash") {
+  REQUIRE(bc::hash("") ==
+          "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855");
+  REQUIRE(bc::hash("x") ==
+          "2d711642b726b04401627ca9fbac32f5c8530fb1903cc4db02258717921a4881");
+  REQUIRE(bc::hash("Satoshi_Nakamoto") ==
+          "175027194c0b650bf9053eabdd1a1bcb02ac41d0c4800ca39a8ab1501a84dcec");
 }
