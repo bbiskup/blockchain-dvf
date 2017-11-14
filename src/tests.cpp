4eeb122e10a4f82471fe48581fa41d6af2d3a509
Bernhard Biskup <bernhard@fract-tech.com>
2017-11-14 21:00:37 +0100

    Added tests

--- src/tests.cpp
+++ src/tests.cpp
@@ -1,4 +1,20 @@
 #define CATCH_CONFIG_MAIN
+#include "blockchain.h"
 #include "vendor/catch.hpp"
 
-TEST_CASE("BlockChain"){};
+TEST_CASE("BlockChain") {
+  bc::BlockChain bc;
+
+  SECTION("Chain") {
+    const bc::Chain& chain{bc.chain()};
+    REQUIRE(chain.size() == 1);
+    REQUIRE(chain.at(0).index == 1);
+  }
+
+  SECTION("Genesis block") {
+    const bc::Block& block{bc.lastBlock()};
+    REQUIRE(block.proof == 100);
+    REQUIRE(block.previousHash == "1");
+    REQUIRE(block.index == 1);
+  }
+};
