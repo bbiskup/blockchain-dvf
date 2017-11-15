4c6d86329e5cfe9285970cd3534ddbeae5d5f1ea
Bernhard Biskup <bernhard@fract-tech.com>
2017-11-15 22:27:36 +0100

    Added API test case

--- src/api_server_test.cpp
+++ src/api_server_test.cpp
@@ -1,6 +1,17 @@
+#include "apiserver.h"
 #include "vendor/catch.hpp"
+#include "vendor/json.hpp"
 
-TEST_CASE("API"){
-    SECTION("mine"){
-    }
+TEST_CASE("Chain") {
+  bc::Server server{5000};
+  SECTION("fullChain") {
+    auto j = nlohmann::json::parse(server.fullChain());
+    nlohmann::json chainJson{j["chain"]};
+    REQUIRE(chainJson.size() == 1);
+    REQUIRE(chainJson[0]["index"] == 1);
+    REQUIRE(chainJson[0]["previous_hash"] == "1");
+    REQUIRE(chainJson[0].find("timestamp") != chainJson[0].end());
+    REQUIRE(chainJson[0]["proof"] == 100);
+    REQUIRE(chainJson[0]["transactions"].size() == 0);
+  }
 }
