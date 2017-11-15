67e7ec9cb5c5ad93d754c60a8be734490b4adf0c
Bernhard Biskup <bernhard@fract-tech.com>
2017-11-15 21:52:29 +0100

    Added assertion

--- src/tests.cpp
+++ src/tests.cpp
@@ -48,7 +48,10 @@ TEST_CASE("hash") {
   REQUIRE(bc::hash("Satoshi_Nakamoto") ==
           "175027194c0b650bf9053eabdd1a1bcb02ac41d0c4800ca39a8ab1501a84dcec");
 }
-TEST_CASE("validProof") { REQUIRE(bc::validProof(100, 35293)); }
+TEST_CASE("validProof") { 
+    REQUIRE(bc::validProof(100, 35293)); 
+    REQUIRE(!bc::validProof(101, 35293)); 
+}
 
 TEST_CASE("Transaction") {
   bc::Transaction transaction{"test_sender", "test_recipient", 100};
